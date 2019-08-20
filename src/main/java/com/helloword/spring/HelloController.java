033bae4ef59a616706e80934e58a843f70b79a52
muhammad-tayyab-confiz <muhammad.tayyab@confiz.com>
2019-08-20 17:59:01 +0500

    - initial commit with hello get api

---
+++ src/main/java/com/helloword/spring/HelloController.java
@@ -0,0 +1,13 @@
+package com.helloword.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "hello";
+    }
+}
